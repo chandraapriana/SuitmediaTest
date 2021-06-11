fe6b92000c4d71dcfe62b4cb2cb9434b1dc620d7
ukk-ostric <aprianachandra@gmail.com>
2021-06-12 01:18:44 +0700

    Create DataDummy.kt

---
+++ app/src/main/java/com/chandra/suitmediatest/utils/DataDummy.kt
@@ -0,0 +1,15 @@
+package com.chandra.suitmediatest.utils
+
+import com.chandra.suitmediatest.R
+
+object DataDummy {
+    fun listImage()=
+        listOf(
+            R.drawable.image1,
+            R.drawable.image2,
+            R.drawable.image3,
+            R.drawable.image4,
+            R.drawable.image5
+        )
+
+}

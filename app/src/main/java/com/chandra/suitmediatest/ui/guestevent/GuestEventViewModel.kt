6d480e6d2cd2d8e326da43fb577193ead92c1b81
ukk-ostric <aprianachandra@gmail.com>
2021-06-12 15:01:04 +0700

    Update GuestEventViewModel.kt

--- app/src/main/java/com/chandra/suitmediatest/ui/guestevent/GuestEventViewModel.kt
+++ app/src/main/java/com/chandra/suitmediatest/ui/guestevent/GuestEventViewModel.kt
@@ -7,16 +7,29 @@ import androidx.lifecycle.ViewModel
 class GuestEventViewModel : ViewModel() {
     private var _name: MutableLiveData<String> = MutableLiveData()
     private var _nameGuest: MutableLiveData<String> = MutableLiveData()
+    private var _nameEvent: MutableLiveData<String> = MutableLiveData()
+    private var _date: MutableLiveData<String>? = MutableLiveData()
+
+
     var name: LiveData<String> = _name
     var nameGuest: LiveData<String> = _nameGuest
+    var nameEvent: LiveData<String> = _nameEvent
+    var date: LiveData<String>? = _date
 
     fun setName(name: String) {
-        this._name.value = name
+        this._name.postValue(name)
     }
 
     fun setNameGuest(nameGuest: String) {
-        this._nameGuest.value = nameGuest
+        this._nameGuest.postValue(nameGuest)
     }
 
+    fun setNameEvent(nameEvent:String){
+        this._nameEvent.postValue(nameEvent)
+    }
+
+    fun setDate(date: String?){
+        this._date?.postValue(date)
+    }
 
 }

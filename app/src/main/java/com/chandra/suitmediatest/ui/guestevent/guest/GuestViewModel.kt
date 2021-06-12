0f753f012444b4fbb2137eab95ec6f94e1513004
ukk-ostric <aprianachandra@gmail.com>
2021-06-12 16:15:29 +0700

    move structure

--- app/src/main/java/com/chandra/suitmediatest/ui/guestevent/guest/GuestViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.chandra.suitmediatest.ui.guestevent.guest
-
-import androidx.lifecycle.ViewModel
-import com.chandra.suitmediatest.data.Repository
-
-class GuestViewModel(private val repository: Repository): ViewModel() {
-    suspend fun getGuest() = repository.getGuest()
-}

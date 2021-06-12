c6847177e03d1df71df8442a4ca87a1537720d13
ukk-ostric <aprianachandra@gmail.com>
2021-06-12 13:15:24 +0700

    add dummy event

--- app/src/main/java/com/chandra/suitmediatest/utils/DataDummy.kt
+++ app/src/main/java/com/chandra/suitmediatest/utils/DataDummy.kt
@@ -1,9 +1,10 @@
 package com.chandra.suitmediatest.utils
 
 import com.chandra.suitmediatest.R
+import com.chandra.suitmediatest.data.model.Event
 
 object DataDummy {
-    fun listImage()=
+    fun listImage() =
         listOf(
             R.drawable.image1,
             R.drawable.image2,
@@ -11,5 +12,34 @@ object DataDummy {
             R.drawable.image4,
             R.drawable.image5
         )
+
+    fun generateDummyEvent(): List<Event> {
+        val listEvent = ArrayList<Event>()
+
+        listEvent.add(
+            Event(
+                "Supertalk with Superstar Education : Information Technology vs Computer Science",
+                "Jumat, 28 Mei 2021",
+                "https://eventbandung.id/wp-content/uploads/2021/05/MP-EB-Supertalk-with-Superstar-Information-Technology-Vs-Computer-Science-%E2%80%93-Which-one-is-right-for-you-Superstar-Education-Jakarta-28-Mei-2021-Copy.jpg"
+            )
+        )
+
+        listEvent.add(
+            Event(
+                "Campus Leaders Gathering : “Pandemi, Susah Cari Kerja?”",
+                "Sabtu, 12 Juni 2021",
+                "https://eventbandung.id/wp-content/uploads/2021/05/MP-EB-Webinar-Pandemi-Susah-Cari-Kerja-Campus-Network-12-Jun-2021-Copy.jpg"
+            )
+        )
+
+        listEvent.add(
+            Event(
+                "Webinar #MYHealthChat “Pengaruh Covid-19 pada Anak Sekolah (Mental & Gizi)”",
+                "Jumat, 28 Mei 2021",
+                "https://eventbandung.id/wp-content/uploads/2021/05/MP-EB-Webinar-Kesehatan-MYHealtChat-Bdgcom_unity-28-Mei-2021-Copy.jpg"
+            )
+        )
+        return listEvent
+    }
 
 }

984eb2f179eb67e2c672d730dc0676d5a6a3f322
ukk-ostric <aprianachandra@gmail.com>
2021-06-13 15:15:34 +0700

    Update DataDummy.kt

--- app/src/main/java/com/chandra/suitmediatest/utils/DataDummy.kt
+++ app/src/main/java/com/chandra/suitmediatest/utils/DataDummy.kt
@@ -20,7 +20,10 @@ object DataDummy {
             Event(
                 "Supertalk with Superstar Education : Information Technology vs Computer Science",
                 "Jumat, 28 Mei 2021",
-                "https://eventbandung.id/wp-content/uploads/2021/05/MP-EB-Supertalk-with-Superstar-Information-Technology-Vs-Computer-Science-%E2%80%93-Which-one-is-right-for-you-Superstar-Education-Jakarta-28-Mei-2021-Copy.jpg"
+                "https://eventbandung.id/wp-content/uploads/2021/05/MP-EB-Supertalk-with-Superstar-Information-Technology-Vs-Computer-Science-%E2%80%93-Which-one-is-right-for-you-Superstar-Education-Jakarta-28-Mei-2021-Copy.jpg",
+                "Hello students! Ada ga sih dari kalian yang bingung mau ambil jurusan IT atau computer science? Pingin belajar coding tapi bingung harus masuk yang mana? Yuk temuin perbedaan jurusan IT dan computer science bersama Clarence, representative dari APU Malaysia.",
+                "-7.061309422077791",
+                "107.74833696749226"
             )
         )
 
@@ -28,7 +31,10 @@ object DataDummy {
             Event(
                 "Campus Leaders Gathering : “Pandemi, Susah Cari Kerja?”",
                 "Sabtu, 12 Juni 2021",
-                "https://eventbandung.id/wp-content/uploads/2021/05/MP-EB-Webinar-Pandemi-Susah-Cari-Kerja-Campus-Network-12-Jun-2021-Copy.jpg"
+                "https://eventbandung.id/wp-content/uploads/2021/05/MP-EB-Webinar-Pandemi-Susah-Cari-Kerja-Campus-Network-12-Jun-2021-Copy.jpg",
+                "Haloo… Sobat Muda Indonesia Campus Leaders Gathering Zoominar Kembali Hadir. Berkolaborasi bersama beberapa Lembaga Anak Muda, yang semangat menghadirkan diskusi hangat dengan tema: PANDEMI, SUSAH CARI KERJA?",
+                "-7.0603736643521415",
+                "107.7500841321331"
             )
         )
 
@@ -36,7 +42,10 @@ object DataDummy {
             Event(
                 "Webinar #MYHealthChat “Pengaruh Covid-19 pada Anak Sekolah (Mental & Gizi)”",
                 "Jumat, 28 Mei 2021",
-                "https://eventbandung.id/wp-content/uploads/2021/05/MP-EB-Webinar-Kesehatan-MYHealtChat-Bdgcom_unity-28-Mei-2021-Copy.jpg"
+                "https://eventbandung.id/wp-content/uploads/2021/05/MP-EB-Webinar-Kesehatan-MYHealtChat-Bdgcom_unity-28-Mei-2021-Copy.jpg",
+                "Live dari Malaysia dan Indonesia, @aradamansara_medicalcentre @malaysiahealthcare @medtourismmy.id x @bdgcom_unity menghadirkan Webinar #MYHealthChat “Pengaruh Covid-19 pada Anak Sekolah (Mental & Gizi)”",
+                "-7.0616338963983205",
+                "107.74720068421578"
             )
         )
 
@@ -44,7 +53,11 @@ object DataDummy {
             Event(
                 "Level Up Class “Wirausaha? Siapa Takut!”",
                 "Jumat, 30 April 2021",
-                "https://eventbandung.id/wp-content/uploads/2021/05/MP-EB-Wirausaha-Siapa-Takut-Level-Up-Class-30-Apr-2021-Copy.jpg"
+                "https://eventbandung.id/wp-content/uploads/2021/05/MP-EB-Wirausaha-Siapa-Takut-Level-Up-Class-30-Apr-2021-Copy.jpg",
+                "Hayo, siapa nih yang udah pengen jadi entrepreneur tapi masih ragu dan bingung mulai dari mana? Nah, kali ini Level Up bakal ajak kalian buat belajar bersama mengenai tantangan jadi entreprenur, bagaimana tips n trick menghadapinya, dan aspek apa saja yang harus diperhatikan. Catat tanggal dan waktunya ya!",
+                "-7.060981264433127",
+                "107.75036592605728"
+
             )
         )
         return listEvent

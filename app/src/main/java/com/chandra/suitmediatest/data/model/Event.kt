f216ede2997a82b5cbc547fe903292a7d10cc049
ukk-ostric <aprianachandra@gmail.com>
2021-06-13 15:15:16 +0700

    Update Event.kt

--- app/src/main/java/com/chandra/suitmediatest/data/model/Event.kt
+++ app/src/main/java/com/chandra/suitmediatest/data/model/Event.kt
@@ -3,6 +3,9 @@ package com.chandra.suitmediatest.data.model
 data class Event(
     val name: String = "",
     val date: String = "",
-    val image: String = ""
+    val image: String = "",
+    val desc : String = "",
+    val latitude:String = "",
+    val longitude:String = ""
 
 )
